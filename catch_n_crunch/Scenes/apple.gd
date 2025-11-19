e0815dbb6bcf4a31ed671c3ee1aad064a0dcc1dd
paper7grid <muddassirsadia123@gmail.com>
2025-11-19 13:51:06 -0800

    created script for apple

---
+++ catch_n_crunch/Scenes/apple.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var fall_speed = 500
+
+signal caught
+
+func _physics_process(delta):
+	position.y += fall_speed * delta
+
+	# If apple goes off-screen → free it
+	if position.y > get_viewport_rect().size.y + 20:
+		queue_free()
