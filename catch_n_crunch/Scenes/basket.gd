07a8dcd7a095f44259b9a6414a9dd688647b4feb
paper7grid <muddassirsadia123@gmail.com>
2025-12-08 21:31:39 -0800

    Added the main game script+apple+basket

--- catch_n_crunch/Scenes/basket.gd
+++ catch_n_crunch/Scenes/basket.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@export var speed: float = 400.0
+@export var speed: float = 900.0
 func _physics_process(delta):
 	var direction = 0
 	
@@ -11,3 +11,6 @@ func _physics_process(delta):
 	move_and_slide()
 	var screen_width = get_viewport().get_visible_rect().size.x
 	position.x = clamp(position.x, 30, screen_width - 30)
+func _on_basket_area_entered(area: Area2D) -> void:
+	if area.is_in_group("apple"):
+		area.emit_signal("caught", area)
--- catch_n_crunch/Scenes/game.gd
+++ catch_n_crunch/Scenes/game.gd
@@ -1,15 +1,28 @@
 extends Node
+var caught_count: int = 0
 @export var apple_scene: PackedScene
+var score: int = 0
+var score_label: Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$appletimer.timeout.connect(spawn_apple)
+	score_label = $Score
 
 func spawn_apple():
 	var apple = apple_scene.instantiate()
 	add_child(apple)
 	
+	
 	var screen_width = get_viewport().size.x
 	apple.position = Vector2(randi() % screen_width, -20)
-	apple.caught.connect(apple_caught)
-func apple_caught():
-	print("Caught!")  # later change to score++
+#func apple_caught():
+	#print("Caught!")  # can add score++
+	
+func apple_caught(apple: Node2D) -> void:
+	score += 1
+	update_score()
+
+func update_score():
+	score_label.text = "Score: " + str(score)
+	
